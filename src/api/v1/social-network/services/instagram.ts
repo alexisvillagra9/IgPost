e954ee981dae229faa76a49b121b3098dd1ef140
Alexis Villagra <alexisvillagra9@gmail.com>
2022-03-22 21:35:23 -0300

    Test error

--- src/api/v1/social-network/services/instagram.ts
+++ src/api/v1/social-network/services/instagram.ts
@@ -4,7 +4,13 @@ export const getPostByHashtagService = async (hashtag: string) => {
   try {
     const response = await axios.get(
       `https://www.instagram.com/explore/tags/${hashtag}/?__a=1`,
-      { proxy: false }
+      {
+        headers: {
+          Cookie:
+            "csrftoken=WCUH1ZuhSe5LRXYSqnIF6Nhf1V6f0ULX; ig_did=85A30C25-36D2-41EA-A1A3-5F69768E9537; ig_nrcb=1; mid=YjpqywAEAAFvn42IvZIMRJD3CQ5G",
+        },
+        proxy: false,
+      }
     );
     const { data: post } = response;
     return post;
