0147ecd35588225a0e08f8974ec00df94e7991f5
Alexis Villagra <alexisvillagra9@gmail.com>
2022-03-22 21:47:50 -0300

    Test error

--- src/api/v1/social-network/services/instagram.ts
+++ src/api/v1/social-network/services/instagram.ts
@@ -6,8 +6,7 @@ export const getPostByHashtagService = async (hashtag: string) => {
       `https://www.instagram.com/explore/tags/${hashtag}/?__a=1`,
       {
         headers: {
-          Cookie:
-            "csrftoken=WCUH1ZuhSe5LRXYSqnIF6Nhf1V6f0ULX; ig_did=85A30C25-36D2-41EA-A1A3-5F69768E9537; ig_nrcb=1; mid=YjpqywAEAAFvn42IvZIMRJD3CQ5G",
+          "Content-Type": "application/json",
         },
         proxy: false,
       }
